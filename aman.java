5e97ede0f3c701d869fd2ef93b028de4b30b3532
AmandeepPandhu <132538945+AmandeepPandhu@users.noreply.github.com>
2023-05-05 08:44:07 +0530

    Create aman.java
    class with java

---
+++ aman.java
@@ -0,0 +1,25 @@
+import java.util.*;
+Class sample
+{
+int a,b;
+{
+void getdata ()
+{
+a=45;
+b=35;
+}
+void show ()
+{
+System.out.println(a+b);
+}
+}
+class aman
+{
+public static void main(String args[])
+{
+sample s=new sample();
+s.getdata();
+s.show();
+}
+}
+
